89f68bcd3ed7879128b53613cad20f4a2ae9eb36
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-09 06:18:06 -0300

    atualizado rollup.config.js com minificação e opções de formato

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import replace from "@rollup/plugin-replace"
+import terser from "@rollup/plugin-terser"
 
 const environment = (process.env.NODE_ENV || "production").trim(),
-    isMinified = environment === "production"
+	isMinified = environment === "production",
+	format = (process.env.REDUX_FILE_FORMAT || "esm").trim()
 
 export default {
 	input: "src/compiler.js",
 	output: {
-		file: "dist/" + "redux-toolkit" + (isMinified ? ".min" : "") + ".js",
-		format: "es"
+		file: "dist/" + "redux-toolkit" + (isMinified ? ".min" : "") + "." + (format === "cjs" ? "c" : "") + "js",
+		format
 	},
 	plugins: [
 		nodeResolve(),
 		replace({
 			"process.env.NODE_ENV": JSON.stringify(environment),
 			preventAssignment: true
-		})
-	]
+		}),
+		isMinified ? terser() : null
+	].filter(Boolean)
 }
