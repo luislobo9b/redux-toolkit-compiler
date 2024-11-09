b34644adde9f40566b1e053fc7fb698957047073
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-09 07:13:23 -0300

    criado compiler-umd.js para compilar com formato umd

---
+++ src/compiler-umd.js
@@ -0,0 +1,9 @@
+import * as RTK from "@reduxjs/toolkit"
+import * as RTKQuery from "@reduxjs/toolkit/query"
+
+window.RTK = RTK
+window.reduxToolkit = RTK
+window.reduxTK = RTK
+window.RTKQuery = RTKQuery
+window.reduxToolkitQuery = RTKQuery
+window.RTKQ = RTKQuery
