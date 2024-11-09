4bcad29a232e8ebc1bf806595abb69ded19205ed
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-09 05:56:18 -0300

    criado compiler.js com o import do redux toolkit e várias nomenclaturas alias

---
+++ src/compiler.js
@@ -0,0 +1,17 @@
+import * as RTK from "@reduxjs/toolkit"
+import * as RTKQuery from "@reduxjs/toolkit/query"
+
+// alias
+const reduxToolkit = RTK,
+	reduxTK = RTK,
+	reduxToolkitQuery = RTKQuery,
+	RTKQ = RTKQuery
+
+export {
+	RTK,
+	reduxToolkit,
+	reduxTK,
+	RTKQuery,
+	reduxToolkitQuery,
+	RTKQ
+}
